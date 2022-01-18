a2ea1d48a6e4c55062d0681c3f58269733f358ff
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-01-18 16:18:55 +0900

    2022-01-18 Java Script코드 삭제

--- project_movie/src/main/resources/static/js/header.js
+++ project_movie/src/main/resources/static/js/header.js
@@ -2,11 +2,6 @@ const menu = document.querySelector(".menu");
 const menu_nav = document.querySelector("#menu_nav");
 const exit_btn = document.querySelector("#exit_btn");
 
-
-
-
-
-
 window.onload = ()=> {
 	menu.onclick =()=> {
 		menu_nav.style.display = "block";
